1d0a5717adf76c84d1a410190599b7d592b58735
Victor Stock Back <victorstock33@gmail.com>
2025-11-05 03:36:58 -0300

    Trazendo tabela para tela de medicos e criando model para medico

---
+++ src/app/core/model/medico.ts
@@ -0,0 +1,8 @@
+export class Medico {
+    id!: number;
+    nome!: string;
+    telefone!: string;
+    crm!: string;
+    plano!: string;
+    area!: string;
+}
--- src/app/pages/medico/medico.component.ts
+++ src/app/pages/medico/medico.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Medico } from '../../core/model/medico';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'vex-medico',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['nome', 'telefone', 'crm', 'area', 'plano'];
+  dataSource!: MatTableDataSource<Medico>;
+  medicos: Medico[] = [];
+  form: FormGroup;
 
-  ngOnInit(): void {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private fb: FormBuilder, private dialog: MatDialog) {
+    this.form = this.fb.group({
+      filtro: [''],
+      categoria: [''],
+      status: ['']
+    });
   }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  ngOnInit(): void {
+    this.medicos = [
+      { id: 1, nome: 'Angelica', telefone: '0500 2025 007', crm: '18541', area:'Pediatrico', plano: 'N' },
+      { id: 2, nome: 'Eliana', telefone: '0500 2025 020', crm: '18542', area:'clinico Geral', plano: 'S' },
+      { id: 3, nome: 'Xuxa', telefone: '0500 2025 040', crm: '18543', area:'Neurologista', plano: 'S' },
+      { id: 4, nome: 'Didi', telefone: '0500 2025 100', crm: '18544', area:'Urologista', plano: 'N' },
+    ];
+    this.dataSource = new MatTableDataSource(this.medicos);
+  }
+   applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
 
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
+
+
+
+
+
